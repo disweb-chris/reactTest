44d53b428a2eb411423e26de83b23865ecbb468b
Christian Henriquez <disweb@gmail.com>
2024-05-31 19:04:48 -0300

    Primer commit usando React

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import ComponenteNuevo from './ComponenteNuevo'
+function App() {
+
+  return (
+    <>
+      <h1>Hola mundo desde React!!</h1>
+      <p>Te queremos Vite y React</p>
+      <ComponenteNuevo></ComponenteNuevo>
+    </>
+  )
+}
+
+export default App
---
+++ src/ComponenteNuevo.jsx
@@ -0,0 +1,5 @@
+function ComponenteNuevo() {
+    return ( <h2>.. Desde el Componente Nuevo ..</h2> );
+}
+
+export default ComponenteNuevo;
